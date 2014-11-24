069180abcd3f1626ce630969141652ad6b72deb3
gipsy <ste3ls@gmail.com>
2014-11-24 20:45:17 +0100

    add prevent-block-place option
    Prevents players from placing blocks outside the world border. Default
    off.
    
    This is for the PrisonPearl plugin, where if a player is imprisoned in a
    chest placed far enough outside the world border it becomes impossible
    to break the chest and free the player.

---
+++ src/main/java/com/wimbli/WorldBorder/cmd/CmdPreventPlace.java
@@ -0,0 +1,37 @@
+package com.wimbli.WorldBorder.cmd;
+
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.wimbli.WorldBorder.Config;
+
+public class CmdPreventPlace extends WBCmd {
+
+	public CmdPreventPlace() {
+		name = permission = "preventblockplace";
+		minParams = maxParams = 1;
+
+		addCmdExample(nameEmphasized() + "<on|off> - turn prevent-block-place on or off.");
+		helpText = "Default value: off. This will prevent players from placing blocks outside the world's border.";
+	}
+	
+	@Override
+	public void cmdStatus(CommandSender sender)
+	{
+		sender.sendMessage(C_HEAD + "preventblockplace is " + enabledColored(Config.whooshEffect()) + C_HEAD + ".");
+	}
+
+	@Override
+	public void execute(CommandSender sender, Player player, List<String> params, String worldName)
+	{
+		Config.setPreventBlockPlace(strAsBool(params.get(0)));
+
+		if (player != null)
+		{
+			Config.log((Config.preventBlockPlace() ? "Enabled" : "Disabled") + " preventblockplace at the command of player \"" + player.getName() + "\".");
+			cmdStatus(sender);
+		}
+	}
+}
